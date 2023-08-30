909fd38121ec2f82b9d349dbb61e93dfdd038332
mahyudin akbar <mahyudinakbar10@gmail.com>
2023-08-30 13:38:55 +0700

    Before Adding DTO

---
+++ learnnest/src/books/books.controller.ts
@@ -0,0 +1,52 @@
+import { Body, Controller, Delete, Get, Param, Post, Put, Query } from '@nestjs/common';
+import { BooksService } from './books.service';
+import { query } from 'express';
+
+@Controller('books')
+export class BooksController {
+    constructor(private booksService: BooksService) { }
+    /* private booksService:BooksService;
+        constructor(booksService:BooksService){
+        this.booksService = booksService
+    }*/
+    @Get()
+    getBooks(
+        @Query('title') title: string,
+        @Query('author') author: string,
+        @Query('category') category: string
+    ) {
+        return this.booksService.getBooks(title, author, category)
+    }
+
+    @Get('/:id')
+    getBook(@Param('id') id: string) {
+        return this.booksService.getBook(id)
+    }
+    @Post()
+    createBook(
+        @Body('title') title: string,
+        @Body('author') author: string,
+        @Body('category') category: string
+    ) {
+        return this.booksService.createBook(title, author, category)
+    }
+
+    @Put('/:id')
+    updateBook(
+        @Param('id') id: string,
+        @Body('title') title: string,
+        @Body('author') author: string,
+        @Body('category') category: string
+    ) {
+        return this.booksService.updateBook(id, title, author, category)
+    }
+
+    @Delete('/:id')
+    deleteBook(
+        @Param('id') id: string
+    ) {
+        return this.booksService.deleteBook(id)
+    }
+
+
+}
